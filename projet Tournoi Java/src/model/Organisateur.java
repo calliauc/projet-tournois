e3b6d09b3598e61394a08e684778a2ea1992bc75
Theo Chastenet <theo.chastenet@gadz.org>
2022-01-24 16:13:16 +0100

    add Java Project

---
+++ projet Tournoi Java/src/model/Organisateur.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Organisateur extends Compte{
+
+	public Organisateur(int idCompte, String pseudo, String password, String mail) {
+		super(idCompte, pseudo, password, mail);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
