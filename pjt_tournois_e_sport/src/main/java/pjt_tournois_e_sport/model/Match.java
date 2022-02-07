50c6ded571462cc1f2642395a589fe8ed88ffc38
Vico <v_luce@yahoo.fr>
2022-02-07 15:38:40 +0100

    Match Jpa version avec journee

--- pjt_tournois_e_sport/src/main/java/pjt_tournois_e_sport/model/Match.java
+++ pjt_tournois_e_sport/src/main/java/pjt_tournois_e_sport/model/Match.java
@@ -1,72 +1,112 @@
 package pjt_tournois_e_sport.model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Match")
+@SequenceGenerator(name="seqMatch", sequenceName = "seq_match",initialValue=100,allocationSize = 1)
 public class Match {
 
 	/// ATTRIBUTES
+	@Id
+	@Column(name="match_id")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqMatch")
+	private Long id;
 	
-	private int idExterieur ; 
-	private int idDomicile ;
-	private int idJournee ; 
-	private int idTournoi ;
+	@OneToMany(mappedBy = "prochainMatch")
+	private List<Inscription> inscriptions;
+	
+	@ManyToOne
+	@JoinColumn(name = "match_journee_id", foreignKey = @ForeignKey(name="inscription_prochain_match_fk"))
+	private Journee journee;
+	
+	@Column(name="resultat")
+	private Map<InscriptionKey,Integer> resultat = new HashMap<InscriptionKey, Integer>();
 	
 	/// CONSTRUCTOR
-	
-	public Match(int idExterieur, int idDomicile, int idJournee, int idTournoi) {
-		this.idExterieur = idExterieur;
-		this.idDomicile = idDomicile;
-		this.idJournee = idJournee;
-		this.idTournoi = idTournoi;
+	public Match() {
 	}
 
+	public Match(List<Inscription> inscriptions, Journee journee, Map<InscriptionKey, Integer> resultat) {
+		super();
+		this.inscriptions = inscriptions;
+		this.journee = journee;
+		this.resultat = resultat;
+	}
+
+	
 	/// GETTERS
 	
-	public int getIdExterieur() {
-		return idExterieur;
+	public Long getId() {
+		return id;
 	}
 
-	public int getIdDomicile() {
-		return idDomicile;
-	}
-	
-	public int getIdJournee() {
-		return idJournee;
+	public List<Inscription> getInscriptions() {
+		return inscriptions;
 	}
 
-	public int getIdTournoi() {
-		return idTournoi;
+	public Journee getJournee() {
+		return journee;
+	}
+
+	public Map<InscriptionKey, Integer> getResultat() {
+		return resultat;
 	}
 
 	
 	/// SETTERS
 	
-	public void setIdExterieur(int idExterieur) {
-		this.idExterieur = idExterieur;
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setInscriptions(List<Inscription> inscriptions) {
+		this.inscriptions = inscriptions;
+	}
+
+	public void setJournee(Journee journee) {
+		this.journee = journee;
+	}
+
+	public void setResultat(Map<InscriptionKey, Integer> resultat) {
+		this.resultat = resultat;
 	}
 
 	
-	public void setIdDomicile(int idDomicile) {
-		this.idDomicile = idDomicile;
-	}
-
-	
-	public void setIdJournee(int idJournee) {
-		this.idJournee = idJournee;
-	}
-
-	
-	public void setIdTournoi(int idTournoi) {
-		this.idTournoi = idTournoi;
-	}
-
-	
-	
+		
 	/// METHODS
 	
 	@Override
-	public String toString() {
-		return "Match [idExterieur=" + idExterieur + ", idDomicile=" + idDomicile + ", idJournee=" + idJournee
-				+ ", idTournoi=" + idTournoi + "]";
-	} 
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Match other = (Match) obj;
+		return Objects.equals(id, other.id);
+	}
 	
 	
 	
