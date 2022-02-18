a917fbc23c7766539758f168f45358f4dab28054
Theo Chastenet <theo.chastenet@gadz.org>
2022-02-18 18:38:54 +0100

    _

--- pjt_tournois_e_sport_boot/src/main/java/projet/sopra/pjt_tournois_e_sport_boot/model/Ligue.java
+++ pjt_tournois_e_sport_boot/src/main/java/projet/sopra/pjt_tournois_e_sport_boot/model/Ligue.java
@@ -3,6 +3,7 @@ package projet.sopra.pjt_tournois_e_sport_boot.model;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
@@ -33,16 +34,9 @@ public class Ligue extends Tournoi {
 	public Ligue() {
 
 	}
-<<<<<<< Updated upstream
-	
 
-	
-	public Ligue(String nom, LocalDate dateDeDebut, String jeu,
-=======
-
-	public Ligue(String nom, LocalDate dateDeCreation, LocalDate dateDeDebut, String jeu,
->>>>>>> Stashed changes
-			Set<Inscription> listeInscriptions, boolean isPoule, int nbPhase) {
+	public Ligue(String nom, LocalDate dateDeDebut, String jeu, Set<Inscription> listeInscriptions, boolean isPoule,
+			int nbPhase) {
 		super(nom, dateDeDebut, jeu, listeInscriptions);
 		this.isPoule = isPoule;
 		this.nbPhase = nbPhase;
@@ -68,90 +62,83 @@ public class Ligue extends Tournoi {
 		this.nbPhase = nbPhase;
 	}
 
-<<<<<<< Updated upstream
 	/// Affichage
-	
+
 	public void displayLigue() {
 		System.out.println("Ligue " + this.idTournoi + " (" + this.listeInscriptions.size() + " players) :");
-		for (Inscription i: this.listeInscriptions) {
+		for (Inscription i : this.listeInscriptions) {
 			System.out.println(i);
 		}
+	}
 
-=======
 	//// METHODS
 
 	public Set<Journee> generateJourneesLigue2PPM() {
+
+		////// TO DO INCLURE LES DATES DE DEBUT/FIN DES MATCHS ET JOURNEES
+
 		Set<Journee> journees = new HashSet<Journee>();
+		LinkedList<Inscription> inscriptionsLigue = new LinkedList<Inscription>(listeInscriptions);
 		for (int i = 0; i < listeInscriptions.size() - 1; i++) {
-
+			Journee j = new Journee();
+			j.setEtape(Etape.Ligue);
 			Set<Match> matchsJournee = new HashSet<Match>();
-			for (int j = 0; j < listeInscriptions.size() / nbParticipantsParMatch; j++) {
-				Match m = new Match();
-				for (int k = 0; k < nbParticipantsParMatch - 1; k++) {
+			for (int k = 0; k < listeInscriptions.size() / 2; k++) {
 
-				}
+				Match m = new Match();
+				m.getInscriptions().add(inscriptionsLigue.get(k));
+				m.getInscriptions().add(inscriptionsLigue.get(k + inscriptionsLigue.size() / 2));
+				matchsJournee.add(m);
 			}
+			j.setMatchsAJouerPourJournee(matchsJournee);
+			journees.add(j);
+
+			inscriptionsLigue.add(1, inscriptionsLigue.pollLast());
 		}
-//		for (int i=0 ; i < participants.size()-1 ;  i++)
-//		{
-//			LinkedList<Match> liste_Match = new LinkedList() ;
-//			for(int j=0 ; j < participants.size()/2; j++)
-//			{
-//				Match m = new Match(participants.get(j).getIdCompte() , participants.get(j+participants.size()/2).getIdCompte(), i, idTournoi ) ;
-//				liste_Match.add(m);
-//			}
-//			
-//			Journee j = new Journee(i, idTournoi, liste_Match) ;
-//			matchs_ligue.add(j);
-//			
-//			participants.add(1,participants.pollLast() ); 
-//			System.out.println(participants);
-//		}
-//		System.out.println(matchs_ligue);
+		System.out.println(journees.toString());
 		return journees;
 	}
 
 	public Set<Journee> generateJourneesLigueMorePPM() {
-				
+		//// METHODE PAS HYPER UTILE MAIS A FINIR POUR LE FUN
 		Set<Journee> journees = new HashSet<Journee>();
-		
+
 		for (int i = 0; i < listeInscriptions.size() - 1; i++) {
 
 			Set<Match> matchsJournee = new HashSet<Match>();
 			for (int j = 0; j < listeInscriptions.size() / nbParticipantsParMatch; j++) {
 				Match m = new Match();
-				
+
 				/// on cree une copie de listeInscription pour la manipuler et la tordre
 				List<Inscription> inscriptionsLigue = new ArrayList<Inscription>(listeInscriptions);
-				while (checkIfMatchUnique(m, journees)==false) {
+				while (checkIfMatchUnique(m, journees) == false) {
 					for (int k = 0; k < nbParticipantsParMatch - 1; k++) {
-						
-							//// on prend des nombres au hasard dans les inscriptions
-							int rand = new Random().nextInt(inscriptionsLigue.size() - k);
-							m.getInscriptions().add(inscriptionsLigue.get(rand)); 
-							inscriptionsLigue.remove(rand);
+
+						//// on prend des nombres au hasard dans les inscriptions
+						int rand = new Random().nextInt(inscriptionsLigue.size() - k);
+						m.getInscriptions().add(inscriptionsLigue.get(rand));
+						inscriptionsLigue.remove(rand);
 					}
-					
+
 				}
 
 			}
 		}
 		return journees;
->>>>>>> Stashed changes
+
 	}
-	
+
 	public boolean checkIfMatchUnique(Match m, Set<Journee> journees) {
-		
+
 		for (Journee j : journees) {
-			for( Match mPrevu : j.getMatchsAJouerPourJournee() ) {
+			for (Match mPrevu : j.getMatchsAJouerPourJournee()) {
 				if (m.getInscriptions().containsAll(mPrevu.getInscriptions())) {
-					return false; 
+					return false;
 				}
-				
+
 			}
 		}
-	
-		
+
 		return true;
 	}
 
