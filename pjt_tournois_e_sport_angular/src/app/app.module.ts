634344cb05caab56f29ebcbff3b91e64c5ae3a05
Diego del Rio <diego.delrio.rodriguez@hotmail.com>
2022-03-04 17:10:55 +0100

    add routes, navig + tournoi (pas fini)

--- pjt_tournois_e_sport_angular/src/app/app.module.ts
+++ pjt_tournois_e_sport_angular/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { routes } from './routes';
+import { TournoiComponent } from './component/tournoi/tournoi.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TournoiComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes), HttpClientModule],
+
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
