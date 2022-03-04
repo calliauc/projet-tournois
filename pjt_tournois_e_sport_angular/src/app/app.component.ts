6fd607af979d14c0e355c6cef99ae04e77f9906d
Diego del Rio <diego.delrio.rodriguez@hotmail.com>
2022-03-04 15:17:53 +0100

    add dossiers de base Angular (avec bootstrap)

---
+++ pjt_tournois_e_sport_angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pjt_tournois_e_sport_angular';
+}
