e12a5ff2eb137838c85e988dc0e528fbc135f177
Diego del Rio <diego.delrio.rodriguez@hotmail.com>
2022-03-09 00:09:14 +0100

    gitflow-feature-stash: resultat_getById_angular

--- pjt_tournois_e_sport_angular/src/app/component/resultat/resultat.component.ts
+++ pjt_tournois_e_sport_angular/src/app/component/resultat/resultat.component.ts
@@ -1,7 +1,9 @@
 import { Observable } from 'rxjs';
+import { Match } from './../../model/match';
 import { ResultatService } from './../../service/resultat.service';
 import { Component, OnInit } from '@angular/core';
 import { Resultat } from 'src/app/model/resultat';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-resultat',
@@ -11,10 +13,33 @@ import { Resultat } from 'src/app/model/resultat';
 export class ResultatComponent implements OnInit {
   resultatsObservable!: Observable<Resultat[]>;
 
-  constructor(private resultatService: ResultatService) {}
+  resultat: Resultat = new Resultat();
+
+  constructor(
+    private resultatService: ResultatService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.resultat.match = new Match();
+  }
 
   ngOnInit(): void {
     this.resultatsObservable = this.resultatService.getAll();
+  }
+
+  idResultat: number = 100;
+  getById(idResultat: number) {
+    this.activatedRoute.params.subscribe((params) => {
+      if (idResultat) {
+        this.resultatService.get(idResultat).subscribe((result) => {
+          console.log(result);
+          this.resultat = result;
+          if (!this.resultat.match) {
+            this.resultat.match = new Match();
+          }
+        });
+      }
+    });
   }
 
   delete(id: number) {
