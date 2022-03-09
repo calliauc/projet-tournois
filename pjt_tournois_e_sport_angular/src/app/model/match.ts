0a9d8475668cdd50ee988963ae6006f88fc65ded
Theo Chastenet <theo.chastenet@gadz.org>
2022-03-09 13:14:21 +0100

    _add MatchService

--- pjt_tournois_e_sport_angular/src/app/model/match.ts
+++ pjt_tournois_e_sport_angular/src/app/model/match.ts
@@ -1,11 +1,25 @@
+import { Resultat } from './resultat';
+import { Journee } from './journee';
 //code pour le test de resultat, match avec seulement un id!!!!
 //reste à faire tous les autres attributs, etc!!!!!
 
+import { Inscription } from './inscription';
+
 export class Match {
   private _id: number | undefined;
-
-  constructor(id?: number) {
+  private _inscriptions: Inscription[] | undefined;
+  private _journee: Journee | undefined;
+  private _resultats: Resultat[] | undefined;
+  constructor(
+    id?: number,
+    inscriptions?: Inscription[],
+    journee?: Journee,
+    resultats?: Resultat[]
+  ) {
     this._id = id;
+    this._inscriptions = inscriptions;
+    this._journee = journee;
+    this._resultats = resultats;
   }
 
   /**
@@ -22,5 +36,26 @@ export class Match {
    */
   public set id(value: number | undefined) {
     this._id = value;
+  }
+
+  public get inscriptions(): Inscription[] | undefined {
+    return this._inscriptions;
+  }
+  public set inscriptions(value: Inscription[] | undefined) {
+    this._inscriptions = value;
+  }
+
+  public get journee(): Journee | undefined {
+    return this._journee;
+  }
+  public set journee(value: Journee | undefined) {
+    this._journee = value;
+  }
+
+  public get resultats(): Resultat[] | undefined {
+    return this._resultats;
+  }
+  public set resultats(value: Resultat[] | undefined) {
+    this._resultats = value;
   }
 }
