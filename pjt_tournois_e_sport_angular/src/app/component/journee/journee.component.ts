efcdfe3f4833b03efb90b7a9e270deea1fa62819
Theo Chastenet <theo.chastenet@gadz.org>
2022-03-09 12:14:55 +0100

    _

--- pjt_tournois_e_sport_angular/src/app/component/journee/journee.component.ts
+++ pjt_tournois_e_sport_angular/src/app/component/journee/journee.component.ts
@@ -1,7 +1,9 @@
+import { async } from '@angular/core/testing';
 import { Journee } from './../../model/journee';
 import { Component, OnInit } from '@angular/core';
 import { JourneeService } from 'src/app/service/journee.service';
 import { Observable } from 'rxjs';
+import { Match } from 'src/app/model/match';
 
 @Component({
   selector: 'app-journee',
