0f80f1b76097a52282c7d2a3ebe61c2599cc4fd8
calliauc <clement@calliau.io>
2022-03-10 15:00:14 +0100

    css

--- pjt_tournois_e_sport_angular/src/app/component/tournoi/details/description/description.component.ts
+++ pjt_tournois_e_sport_angular/src/app/component/tournoi/details/description/description.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Championnat } from 'src/app/model/championnat';
+import { Tournoi } from 'src/app/model/tournoi';
 
 @Component({
   selector: 'app-description',
   templateUrl: './description.component.html',
-  styleUrls: ['./description.component.css']
+  styleUrls: ['./description.component.css'],
 })
 export class DescriptionComponent implements OnInit {
+  @Input()
+  tournoi: Tournoi = new Championnat();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
