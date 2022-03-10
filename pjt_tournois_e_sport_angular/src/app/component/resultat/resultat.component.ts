0ac9530df26626f04f0c5fba8ad7fb6c8e790881
Diego del Rio <diego.delrio.rodriguez@hotmail.com>
2022-03-10 23:18:48 +0100

    trier resultat par ordre croissant et decroissant

--- pjt_tournois_e_sport_angular/src/app/component/resultat/resultat.component.ts
+++ pjt_tournois_e_sport_angular/src/app/component/resultat/resultat.component.ts
@@ -4,6 +4,8 @@ import { ResultatService } from './../../service/resultat.service';
 import { Component, OnInit } from '@angular/core';
 import { Resultat } from 'src/app/model/resultat';
 import { ActivatedRoute, Router } from '@angular/router';
+import { concatMap, filter, map } from 'rxjs/operators';
+import { Inscription } from './../../model/inscription';
 
 @Component({
   selector: 'app-resultat',
@@ -14,6 +16,8 @@ export class ResultatComponent implements OnInit {
   resultatsObservable!: Observable<Resultat[]>;
 
   resultat: Resultat = new Resultat();
+
+  resultats: Resultat[] = [];
 
   constructor(
     private resultatService: ResultatService,
@@ -46,5 +50,57 @@ export class ResultatComponent implements OnInit {
     this.resultatService.delete(id).subscribe((ok) => {
       this.resultatsObservable = this.resultatService.getAll();
     });
+  }
+
+  orderDesc() {
+    this.resultatsObservable = this.resultatService
+      .getAll()
+      .pipe(
+        map((resultats) =>
+          resultats
+            .filter((item): item is Resultat => !!item)
+            .sort((a, b) => (a.id < b.id ? 1 : -1))
+        )
+      );
+
+    interface Resultat {
+      _id: number;
+      _match: Match;
+      _participant: Inscription;
+      _positionMatch: number;
+      _scoreMatch: number;
+
+      id: number;
+      match: Match;
+      participant: Inscription;
+      positionMatch: number;
+      scoreMatch: number;
+    }
+  }
+
+  orderAsc() {
+    this.resultatsObservable = this.resultatService
+      .getAll()
+      .pipe(
+        map((resultats) =>
+          resultats
+            .filter((item): item is Resultat => !!item)
+            .sort((a, b) => (a.id > b.id ? 1 : -1))
+        )
+      );
+
+    interface Resultat {
+      _id: number;
+      _match: Match;
+      _participant: Inscription;
+      _positionMatch: number;
+      _scoreMatch: number;
+
+      id: number;
+      match: Match;
+      participant: Inscription;
+      positionMatch: number;
+      scoreMatch: number;
+    }
   }
 }
